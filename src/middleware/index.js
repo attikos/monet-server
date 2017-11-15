b5114c4fda415ff726a6aef0463d68698c4492f4
Maksim Bylkov <fearcrymaks@gmail.com>
2017-11-16 01:54:56 +0400

    auth, signup hooks, added logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,14 @@
+const handler = require('feathers-errors/handler');
+const notFound = require('./not-found-handler');
+const logger = require('./logger');
+
 module.exports = function () {
   // Add your custom middleware here. Remember, that
   // in Express the order matters, `notFound` and
   // the error handler have to go last.
   const app = this; // eslint-disable-line no-unused-vars
+
+  app.use(notFound());
+  app.use(logger(app));
+  app.use(handler());
 };
--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,18 +1,22 @@
 const { authenticate } = require('feathers-authentication').hooks;
+const { hashPassword } = require('feathers-authentication-local').hooks;
+const commonHooks  = require('feathers-hooks-common');
+// const gravatar = require('../../hooks/gravatar');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    all: [],
+    find: [ authenticate('jwt') ],
+    get: [ authenticate('jwt') ],
+    // create: [hashPassword(), gravatar()],
+    create: [ hashPassword() ],
+    update: [ authenticate('jwt') ],
+    patch: [ authenticate('jwt') ],
+    remove: [ authenticate('jwt') ]
   },
 
   after: {
-    all: [],
+    all: [ commonHooks.when( hook => hook.params.provider, commonHooks.discard('password') ) ],
     find: [],
     get: [],
     create: [],
